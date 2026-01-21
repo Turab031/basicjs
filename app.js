edd95594385f56557899c9aaddcdf9c3f30d579e
Turab031 <147339712+Turab031@users.noreply.github.com>
2026-01-21 12:17:09 +0530

    hell

---
+++ app.js
@@ -0,0 +1,18 @@
+// const hello=()=>{
+//     console.log("hello world")
+// }
+
+// setTimeout(hello,3000)
+
+// callback hell
+
+let age = 19;
+if (age >= 18) {
+  if (age >= 50) {
+    console.log("senior");
+  } else {
+    console.log("middle");
+  }
+} else {
+  console.log("child");
+}

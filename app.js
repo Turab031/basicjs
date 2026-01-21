e59401726b175e82193311eacb9f0fe8c29babe5
Turab031 <147339712+Turab031@users.noreply.github.com>
2026-01-21 21:08:47 +0530

    cb

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-// const hello=()=>{
-//     console.log("hello world")
+// // const hello=()=>{
+// //     console.log("hello world")
+// // }
+
+// // setTimeout(hello,3000)
+
+// // callback hell
+// //
+// let age = 19;
+// if (age >= 18) {
+//   if (age >= 50) {
+//     console.log("senior");
+//   } else {
+//     console.log("middle");
+//   }
+// } else {
+//   console.log("child");
 // }
 
-// setTimeout(hello,3000)
+// // callback helll 
+// function getdata(dataid, getnextdata) {
+//   setTimeout(() => {
+//     console.log("data", dataid);
+//     if (getnextdata) {
+//       getnextdata();
+//     }
+//   }, 2000);
+// }
 
-// callback hell
+// getdata(1, () => {
+//   console.log("gettig data....");
+//   getdata(2, () => {
+//     console.log("gettig data....");
+//     getdata(3, () => {
+//       console.log("gettig data....");
+//       getdata(4, () => {
+//         console.log("gettig data....");
+//         getdata(5);
+//       });
+//     });
+//   });
+// });
 
-let age = 19;
-if (age >= 18) {
-  if (age >= 50) {
-    console.log("senior");
-  } else {
-    console.log("middle");
-  }
-} else {
-  console.log("child");
-}
+
+// to solve this->promises
+
